f23348ea4b3f52405dd86adb2ba6b35c5944a8f2
akhileshit <akhil.11320@gmail.com>
2025-08-15 00:25:55 +0530

    some random commit near AdminHome to continue with them branch..

--- src/components/AdminProductMgmt.jsx
+++ src/components/AdminProductMgmt.jsx
@@ -1,17 +1,171 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function AdminProductMgmt() {
+export default function AdminProductMgmt({ operation }) {
+  const [isDelete, setIsDelete] = useState(false)
+  const [isAdd, setIsAdd] = useState(false)
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState(0.0);
+  const [stock, setStock] = useState(0);
+  const [categoryId, setCategoryId] = useState(0);
+  const [imageUrl, setImageUrl] = useState('');
+  const [productId, setProductId] = useState(0)
+  const [error, setError] = useState(null)
+  const [message, setMessage] = useState(null)
 
-    const addProduct = (productDetails) => {
-        
+  useEffect(() => {
+    setError(null) // Clear any errors or messages if operation changes
+    setMessage(null)
+
+    if (operation === 'ADD') { 
+      setIsDelete(false);
+      setIsAdd(true); 
     }
-
-    const deleteProduct = (productId) => {
-
+    else if (operation === 'DELETE') { 
+      setIsAdd(false);
+      setIsDelete(true); 
     }
+  }, [operation])
+
+  const addProduct = async (e) => {
+    e.preventDefault()
+    setError(null)
+    setMessage(null)
+
+    try {
+      const response = await fetch('http://localhost:9000/admin/products/add', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        credentials: 'include',
+        body: JSON.stringify({ name, description, price, stock, categoryId, imageUrl })
+      })
+      setName(''); setDescription(''); setPrice(0); setStock(0); setCategoryId(0); setImageUrl(''); // Clear input fields
+      const data = await response.json()
+
+      if (response.status === 201) {
+        console.log("Product added successfully.")
+        setMessage("Product added successfully.")
+        // still "data" is there. use it later...........
+      } else {
+        throw new Error(data.error)
+      }
+    } catch (err) {
+      console.log(err.message)
+      setError(err.message)
+    }
+  }
+
+  const deleteProduct = async (e) => {
+    e.preventDefault()
+    setError(null)
+    setMessage(null)
+
+    try {
+      const response = await fetch('http://localhost:9000/admin/products/delete', {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        credentials: 'include',
+        body: JSON.stringify({ productId }),
+      })
+      setProductId(0)  // Clear input fields
+      const data = await response.json()
+      if (response.ok) {
+        console.log(data)
+        setMessage(data.message)
+      } else {
+        throw new Error(data.error)
+      }
+
+    } catch (err) {
+      console.log(err.message)
+      setError(err.message)
+    }
+  }
   return (
-    <div>
-        
+    <div className='product-management-page'>
+      {isAdd &&
+        <div className="form-container admin-form">
+          <form onSubmit={addProduct} className="form-content">
+            <h1 className="form-header">ADD PRODUCT</h1>
+            {error && <span className='error-message'>{error}</span>}
+            {message && <span className='message'>{message}</span>}
+            <div className="form-group">
+              <label htmlFor="name" className="form-label">Name</label>
+              <input type="text" className='form-input' id='name'
+                placeholder='Enter product name'
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="description" className="form-label">Description</label>
+              <input type="text" className='form-input' id='description'
+                placeholder='Enter product description'
+                required
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="price" className="form-label">Price</label>
+              <input type="number" className='form-input' id='price'
+                placeholder='Enter product price'
+                required
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="stock" className="form-label">Stock</label>
+              <input type="number" className='form-input' id='stock'
+                placeholder='Enter product stock'
+                required
+                value={stock}
+                onChange={(e) => setStock(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="categoryId" className="form-label">Category Id</label>
+              <input type="number" className='form-input' id='categoryId'
+                placeholder='Enter product categoryId'
+                required
+                value={categoryId}
+                onChange={(e) => setCategoryId(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="imageUrl" className="form-label">Image Url</label>
+              <input type="text" className='form-input' id='imageUrl'
+                placeholder='Enter product imageUrl'
+                required
+                value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+              />
+            </div>
+            <button className="form-button">ADD</button>
+          </form>
+        </div>
+      }
+      {isDelete &&
+        <div className="form-container admin-form">
+          <form onSubmit={deleteProduct} className="form-content">
+            <h1 className="form-header">DELETE PRODUCT</h1>
+            {error && <span className='error-message'>{error}</span>}
+            {message && <span className='message'>{message}</span>}
+            <div className="form-group">
+              <label htmlFor="productId" className="form-label">Product Id</label>
+              <input type="text" className='form-input' id='productId'
+                placeholder='Enter product id'
+                required
+                value={productId}
+                onChange={(e) => setProductId(e.target.value)}
+              />
+            </div>
+            <button className="form-button">DELETE</button>
+          </form>
+        </div>
+      }
     </div>
   )
 }
--- src/pages/AdminHome.jsx
+++ src/pages/AdminHome.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AdminProductMgmt from '../components/AdminProductMgmt'
 
 export default function AdminHome() {
+  const [operation, setOperation] = useState('')
+  const [isClosed, setIsClosed] = useState(true)
+
+  const handleOnAdd = (e) => {
+    e.preventDefault()
+    setIsClosed(false)
+    setOperation('ADD')
+  }
+  const handleOnDelete = (e) => {
+    e.preventDefault()
+    setIsClosed(false)
+    setOperation('DELETE')
+  }
+  const handleClose = (e) => {
+    e.preventDefault()
+    setIsClosed(true)
+  }
+
   return (
-    <div>
-        AdminHome
+    <div className='admin-homepage'>
+      <div className="management-section">
+        <h1 className="section-header"><span>Product Management</span></h1>
+        <div className="admin-cards">
+          <div className="admin-card">
+            <div className="card-title">Add Product</div>
+            <p className="card-description">Add product to the database</p>
+            <button className="card-button" onClick={handleOnAdd}>Enter Product Details</button>
+          </div>
+          <div className="admin-card">
+            <div className="card-title">Delete Product</div>
+            <p className="card-description">Delete product from the database</p>
+            <button className="card-button" onClick={handleOnDelete}>Enter Product Details</button>
+          </div>
+        </div>
+        {operation && !isClosed &&
+          <div className="admin-product-mgmt-page">
+            <button className="close-button" onClick={handleClose}>Close</button>
+            <AdminProductMgmt operation={operation}/>
+          </div>
+        }
+      </div>
     </div>
   )
 }
